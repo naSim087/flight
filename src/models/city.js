37ffb16592de0ded570ebfd23b6cdcd3014fee53
naSim087 <nasimsheikh688@gmail.com>
2024-04-19 01:01:36 +0530

    added the airport

--- src/models/city.js
+++ src/models/city.js
@@ -4,13 +4,12 @@ const {
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class City extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
+   
     static associate(models) {
-      // define association here
+     // here we mention all the associations
+     this.hasMany(models.Airport,{
+      foreignKey:'cityId'
+     });
     }
   }
   City.init({
